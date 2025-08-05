a7520816e2dd11cd96f318b749ff527bb7d10f5f
Daniil Ivanchin <moustachebookie@gmail.com>
2025-08-05 19:50:13 +0500

    create def test_click_site and def test_enter_password

--- test_steam_site.py
+++ test_steam_site.py
@@ -1,4 +1,7 @@
+import time
+
 import pytest
+import string
 import random
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,4 +12,14 @@ driver = webdriver.Chrome(service=service)
 
 def test_enter_site():
     driver.get("https://store.steampowered.com/")
-    assert "Steam" in driver.title
+    assert "https://store.steampowered.com/" in driver.current_url
+
+def test_click_site():
+    driver.find_elements("class name", "global_action_link")[0].click()
+    driver.implicitly_wait(2)
+    assert "login" in driver.current_url
+
+def test_enter_password():
+    driver.find_elements("class name", "_2GBWeup5cttgbTw8FM3tfx")[0].send_keys(''.join(random.choices(string.ascii_letters + string.digits, k=8)))
+    driver.find_elements("class name", "_2GBWeup5cttgbTw8FM3tfx")[1].send_keys(''.join(random.choices(string.ascii_letters + string.digits, k=8)))
+    time.sleep(3)
